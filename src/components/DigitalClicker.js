83ecfbee82a3fd1f38d6cab3bf62983275af6bad
Maloochow <luyao.mly@gmail.com>
2020-09-29 10:14:20 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+class DigitalClicker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    render() {
+    return <button onClick={this.handleClick} >{this.state.timesClicked}</button>
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,59 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+class YouTubeDebugger extends Component {
+    constructor() {
+        super();
+        this.state =  {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    render() {
+        return (
+        <div>
+        <button onClick={this.handleBitrate} className="bitrate" >bitrate</button>
+        <button onClick={this.handleClick2} className="resolution" >resolution</button>
+        </div>
+        )
+    }
+
+    handleBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12    
+            }
+        })
+    }
+    // handleClick1 = () => {
+    //     this.setState(previousState => {
+    //         return {
+    //         settings: {
+    //             ...previousState.settings,
+    //             bitrate: 12    
+    //         }
+    //     }
+    //     })
+    // }
+
+    handleClick2 = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }    
+            }
+        })
+    }
+}
+
+export default YouTubeDebugger
